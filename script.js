1b95fee0f8e6a425b028a933508abef77c08338f
Emelinda matos <smiles0721@gmail.com>
2022-02-19 20:47:55 -0500

    finshed buttons

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
-//this stores the votes
-let dogVotesCount = 0
-let catVotesCount = 0
-let fishVotesCount = 0
+let dogVotescount = 0
+let catVotescount = 0
+let fishVotescount = 0
 
-//get reference to the td element with vote count for dog
-let votesForDogTd = document.querySelector("#doggo-votes")
-let votesForCatTd = document.querySelector("#catto-votes")
-let votesForFishTd = document.querySelector("#fish-votes")
+let votesforDogTd = document.querySelector("#doggo-votes")
+let votesforCatTd = document.querySelector("#catto-votes")
+let votesforFishTd = document.querySelector("#fish-votes")
 
-console.log("Votes for dog td", votesForDogTd)
-console.log("Votes for cat td", votesForCatTd)
-console.log("Votes for fish td", votesForFishTd)
+console.log("Votes for dog td", votesforDogTd)
+console.log("Votes for cat td", votesforCatTd)
+console.log("Votes for fish td", votesforFishTd)
 
-//first get reference to button
+// refence for button
 let dogButton = document.querySelector("#dog-button")
 
-//register click listeners when buttons are clicked
-dogButton.addEventListener("click", function () {
-    // window.alert("Dog button clicked")
-    // add one to the total dog votes
-    dogVotesCount = dogVotesCount + 1;
+// click function
+dogButton.addEventListener("click", function(){
+    //window.alert("Dog button clicked!")
+    dogVotescount = dogVotescount + 1;
 
-    //update the td element to have the right count
-    votesForDogTd.textContent = dogVotesCount
+    votesforDogTd.textContent = dogVotescount
 })
-
-//refrence cat button
+//
 let catButton = document.querySelector("#cat-button")
-//register listener
-catButton.addEventListener("click", function () {
-    // add one to the total cat votes
-    catVotesCount = catVotesCount + 1;
 
-    //update the td element to have the right count
-    votesForCatTd.textContent = catVotesCount
+catButton.addEventListener("click", function(){
+    catVotescount = catVotescount + 1;
+
+    votesforCatTd.textContent = catVotescount
 })
-
-//ref fish button
+//
 let fishButton = document.querySelector("#fish-button")
-//register listener
-fishButton.addEventListener("click", function () {
-    
-    // add one to the total fish votes
-    fishVotesCount = fishVotesCount + 1;
 
-    //update the td element to have the right count
-    votesForFishTd.textContent = fishVotesCount
+// click function
+fishButton.addEventListener("click", function(){
+    //window.alert("Dog button clicked!")
+    fishVotescount = fishVotescount + 1;
+
+    votesforFishTd.textContent = fishVotescount
 })
